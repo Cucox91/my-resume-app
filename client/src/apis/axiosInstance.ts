1552967fc6fdbe036e17be8f3a00e7de13dc1123
Raziel Arias <raziel.arias1991@gmail.com>
2025-03-28 22:46:57 -0400

    Trying to fix Backend Path

--- client/src/apis/axiosInstance.ts
+++ client/src/apis/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "https://localhost:5001", //Raziel: In the future fix this for azure. This is taking the hardcoded value for now.
+  baseURL: import.meta.env.VITE_BACKEND_URL || "", //Raziel: In the future fix this for azure. This is taking the hardcoded value for now.
 });
 
 export default axiosInstance;
